69a9ae15b1f08d2c2dcd17ab6411942dd44e5ff9
Gintoki <31125160+G1n7oki@users.noreply.github.com>
2020-12-11 08:51:10 +0800

    Update Methods.js

--- Methods.js
+++ Methods.js
@@ -354,9 +354,28 @@ const bubbleSort = arr => {
     for (let j = 0; j < len - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         const temp = arr[j + 1]
-	arr[j + 1] = arr[j]
-	arr[j] = temp
+				arr[j + 1] = arr[j]
+				arr[j] = temp
       }
     }
   }
+	return arr
+}
+
+// 选择排序
+const selectedSort = arr => {
+	const len = arr.length
+	let min, temp
+	for (let i = 0; i < len - 1; i++) {
+		min = i
+		for (let j = i + 1; j < len; j++) {
+			if (arr[j] < arr[min]) {
+				min = j
+			}
+		}
+		temp = arr[i]
+    arr[i] = arr[min]
+    arr[min] = temp
+	}
+	return arr
 }
