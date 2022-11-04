c61a31be1ddaf9f1c66d3d6aa382a3791634809b
Damn <G1n7oki@163.com>
2022-11-04 16:03:42 +0800

    refactor: Update

--- index.js
+++ index.js
@@ -1014,5 +1014,33 @@ const shellSort = arr => {
   return arr
 }
 
+/**
+ * 匹配有效的括号
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = (s) => {
+  const n = s.length
+  if (n % 2 === 1) {
+    return false
+  }
+  const pairs = new Map([
+    [')', '('],
+    [']', '['],
+    ['}', '{']
+  ])
+  const stk = []
+  for (let ch of s) {
+    if (pairs.has(ch)) {
+      if (!stk.length || stk[stk.length - 1] !== pairs.get(ch)) {
+        return false
+      }
+      stk.pop()
+    } else {
+      stk.push(ch)
+    }
+  }
+  return !stk.length
+}
 
 
